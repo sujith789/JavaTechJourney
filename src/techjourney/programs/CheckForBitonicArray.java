ce25fee7a64dc9bfe5081175dd357b7c8df6ec3e
sujithmo <sujithperuva@yahoo.com>
2015-07-21 05:54:54 -0700

    Java program to check whether an array is bitonic Added some test cases

--- src/techjourney/programs/CheckForBitonicArray.java
+++ src/techjourney/programs/CheckForBitonicArray.java
@@ -15,7 +15,8 @@ public class CheckForBitonicArray {
     {
         System.out.println(checkBitonicArray(new int[]{0,1,2,3,10,9,8,7,6})); //bitonic -> true
         System.out.println(checkBitonicArray(new int[]{0,1,2,3,10,9})); //bitonic -> true
-        System.out.println(checkBitonicArray(new int[]{5,4,3,2,1})); //bitonic -> true
+        System.out.println(checkBitonicArray(new int[]{5,4,3,2,1})); //bitonic -> false
+        System.out.println(checkBitonicArray(new int[]{1,3,5})); //bitonic -> false
     }
 
     public static Boolean checkBitonicArray(int arr[])
@@ -47,7 +48,7 @@ public class CheckForBitonicArray {
             }
         }
 
-        for(int i=temp1+1;i<arr.length;i++)
+        for(int i=temp1;i<arr.length;i++)
         {
             if(arr[i]-temp2>0)
             {
